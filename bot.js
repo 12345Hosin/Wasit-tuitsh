f22ffb57e3128bfcf50f643cc77456eef7933969
12345Hosin <51540395+12345Hosin@users.noreply.github.com>
2019-07-05 20:31:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,9 +8,9 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`وسيط نايم`,"http://twitch.tv/وسيط نايم")
+client.user.setGame(`سلوم`,"http://twitch.tv/سلوم")
 client.user.setStatus("dnd")
 });
 
-client.login('NTk1NzIwODQ0NDIwOTcyNTYz.XRvNug.WapCJKQEaTOphoL8ObtYe30Az6k'
+client.login('NTgxMjEyMzMzMzcyNzM1NjA2.XRjzRw.AL1BLpy9HiqeHZpFks0muUBj6hI'
 );// لا تغير فيها شيء
